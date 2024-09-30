81b7270a9baba8fcf8afa3bacd266d8406e55a0d
Cllano21 <cllano21@gmail.com>
2024-09-30 09:39:08 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -15,3 +15,19 @@ document.querySelectorAll('.nav-link').forEach(link => {
         });
     });
 });
+
+const hiddenImageSrc = './img4.png'; // Cambia esto a la ruta correcta
+const img = new Image();
+img.src = hiddenImageSrc;
+
+img.onload = function() {
+    // Ahora puedes agregar la imagen oculta al PDF
+    html2pdf().from(content).set(options).toPdf().get('pdf').then((pdf) => {
+        pdf.addImage(img, 'PNG', 15, 170, 180, 160); // Ajusta la posición y tamaño según necesites
+        pdf.save('pagina-con-imagen.pdf');
+    });
+};
+
+if (img.complete) {
+    img.onload(); // Llama la función si la imagen ya está cargada
+}
